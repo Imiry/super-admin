76fd19017a1eb63fdb1f6cc4e1a7ea3ae1c57481
sitaoi <tao.si@wyze.cn>
2022-08-12 18:08:06 +0800

    feat:    新功能: 完成图标和登录页

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+// 导入全局样式
+import './styles/index.scss'
+// 导入 svgIcon
+import installIcons from '@/icons'
+
+const app = createApp(App)
+installIcons(app)
+
+app.use(store).use(router).use(ElementPlus).mount('#app')
---
+++ src/views/login/rules.ts
@@ -0,0 +1,11 @@
+// import i18n from '@/i18n'
+export const validatePassword = () => {
+  return (rule:any, value:any, callback:any) => {
+    if (value.length < 6) {
+      callback(new Error(''))
+      // callback(new Error(i18n.global.t('msg.login.passwordRule')))
+    } else {
+      callback()
+    }
+  }
+}
